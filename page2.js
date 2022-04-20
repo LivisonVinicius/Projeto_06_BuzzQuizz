42c0ba7a93637c479d4c791a735383462a47a92f
LivisonVinicius <livisongc@gmail.com>
2022-04-20 15:44:51 -0300

    Muda cor quando certo ou errado

--- page2.js
+++ page2.js
@@ -3,13 +3,18 @@ let counterRespostas;
 let salvaPromisse;
 let respostaSelector;
 let promise;
+let questionSelector;
 function geraQuiz(){
     promise=axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes')
     promise.then(function(response){
+        body.innerHTML = `
+        <header><h1>BuzzQuizz</h1></header>
+        <main class="page2"></main>
+        `
         counter=0;
         counterRespostas=0;
         let posicaoID=0
-        let questionSelector=response.data[posicaoID].questions;
+        questionSelector=response.data[posicaoID].questions;
         document.querySelector(".page2").innerHTML+=`
         <li class="quizzTitle">
             <img src="${response.data[posicaoID].image}" alt="Titulo do quizz">
@@ -30,10 +35,9 @@ function geraQuiz(){
             while(counterRespostas < respostaSelector.length){
                 pergunta=document.querySelector(`.pergunta${counter}`);
                 pergunta.innerHTML +=`
-                <div class="resposta${counter} resposta" onclick="selecionaResposta(this,'pergunta${counter}')">
+                <div class="resposta${counterRespostas} resposta" onclick="selecionaResposta(this,'pergunta${counter}')">
                     <img src="${respostaSelector[counterRespostas].image}" alt="opção${counterRespostas}">
                     <h4>${respostaSelector[counterRespostas].text}</h4>
-                    <div class="esbranquicado escondido"></div>
                 </div>
                 `
                 counterRespostas++
@@ -45,11 +49,22 @@ function geraQuiz(){
 }
 function selecionaResposta(selecionada,localPergunta){
     let contadorLoop=0
-    while(contadorLoop < document.querySelectorAll(`.${localPergunta} > *`).length-1){
-        document.querySelector(`.${localPergunta} .resposta${contadorLoop}`).classList.("esbraquicado")
-        contadorLoop++
-
+    let posicaoPergunta=localPergunta.slice(8)
+    if(document.querySelectorAll(`.${localPergunta} .esbraquicado`).length != 0){
+        return
     }
+    while(contadorLoop < document.querySelectorAll(`.${localPergunta} > *`).length-1){
+        document.querySelector(`.${localPergunta} .resposta${contadorLoop}`).classList.add("esbraquicado")
+        switch(questionSelector[Number(posicaoPergunta)].answers[contadorLoop].isCorrectAnswer){
+            case true:
+                document.querySelector(`.${localPergunta} .resposta${contadorLoop}`).classList.add("truthy")
+                break
+            case false:
+                document.querySelector(`.${localPergunta} .resposta${contadorLoop}`).classList.add("falsy")
+        }
+        contadorLoop++
+    }
+    selecionada.classList.remove("esbraquicado")
 }
 function parametro(){
     return Math.random()-0.5;
