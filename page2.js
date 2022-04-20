9593983758022d25dd0023a26692602e7a1f2ae1
LivisonVinicius <livisongc@gmail.com>
2022-04-20 13:52:01 -0300

    Trabalhando na seleção de resposta

--- page2.js
+++ page2.js
@@ -1,23 +1,22 @@
-let promise;
 let counter;
 let counterRespostas;
 let salvaPromisse;
 let respostaSelector;
-
+let promise;
 function geraQuiz(){
-    const promise=axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes')
+    promise=axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes')
     promise.then(function(response){
-        document.querySelector(".page2").innerHTML+=`
-        <li class="quizzTitle">
-            <img src="hogwarts-castle-.jpg" alt="qualquersa">
-            <div><h2>O quão Potterhead você é?</h2></div>
-        </li>
-        <div class="quizzContainer"></div>
-        `
         counter=0;
         counterRespostas=0;
         let posicaoID=0
         let questionSelector=response.data[posicaoID].questions;
+        document.querySelector(".page2").innerHTML+=`
+        <li class="quizzTitle">
+            <img src="${response.data[posicaoID].image}" alt="Titulo do quizz">
+            <div><h2>${response.data[posicaoID].title}</h2></div>
+        </li>
+        <div class="quizzContainer"></div>
+        `
         let containerQuiz=document.querySelector(".page2 .quizzContainer");
         while (counter < questionSelector.length){
             counterRespostas=0
@@ -26,13 +25,15 @@ function geraQuiz(){
                 <h3>${questionSelector[counter].title}</h3>
             </li>
             `
+            document.querySelector(`.pergunta${counter} h3`).style.backgroundColor =`${response.data[posicaoID].questions[counter].color}`
             respostaSelector=questionSelector[counter].answers.sort(parametro);
             while(counterRespostas < respostaSelector.length){
                 pergunta=document.querySelector(`.pergunta${counter}`);
                 pergunta.innerHTML +=`
-                <div>
+                <div class="resposta${counter} resposta" onclick="selecionaResposta(this,'pergunta${counter}')">
                     <img src="${respostaSelector[counterRespostas].image}" alt="opção${counterRespostas}">
                     <h4>${respostaSelector[counterRespostas].text}</h4>
+                    <div class="esbranquicado escondido"></div>
                 </div>
                 `
                 counterRespostas++
@@ -41,6 +42,14 @@ function geraQuiz(){
         }
         counter=0
     })
+}
+function selecionaResposta(selecionada,localPergunta){
+    let contadorLoop=0
+    while(contadorLoop < document.querySelectorAll(`.${localPergunta} > *`).length-1){
+        document.querySelector(`.${localPergunta} .resposta${contadorLoop}`).classList.("esbraquicado")
+        contadorLoop++
+
+    }
 }
 function parametro(){
     return Math.random()-0.5;
