42e21090c5655ee518263044c994ea3504c5a8b7
LivisonVinicius <livisongc@gmail.com>
2022-04-19 22:21:13 -0300

    Funcionalidade integrada com o api

--- page2.js
+++ page2.js
@@ -1,180 +1,36 @@
-let response2;
 let promise;
-let objeto;
 let counter;
 let counterRespostas;
+let salvaPromisse;
 function geraQuiz(){
-    // const promise=axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes')
-    // promise.then(function(response){
-    //     response2=response.data
-    //     const containerQuiz=document.querySelector(".quiz")
-    // })
-    objeto=[
-        {
-            id: 1,
-            title: "Título do quizz1",
-            image: "https://http.cat/411.jpg",
-            questions: [
-                {
-                    title: "Título da pergunta 1",
-                    color: "#123456",
-                    answers: [
-                        {
-                            text: "Texto da resposta 1",
-                            image: "https://http.cat/411.jpg",
-                            isCorrectAnswer: true
-                        },
-                        {
-                            text: "Texto da resposta 2",
-                            image: "https://http.cat/412.jpg",
-                            isCorrectAnswer: false
-                        }
-                    ]
-                },
-                {
-                    title: "Título da pergunta 2",
-                    color: "#123456",
-                    answers: [
-                        {
-                            text: "Texto da resposta 1",
-                            image: "https://http.cat/411.jpg",
-                            isCorrectAnswer: true
-                        },
-                        {
-                            text: "Texto da resposta 2",
-                            image: "https://http.cat/412.jpg",
-                            isCorrectAnswer: false
-                        }
-                    ]
-                },
-                {
-                    title: "Título da pergunta 3",
-                    color: "#123456",
-                    answers: [
-                        {
-                            text: "Texto da resposta 1",
-                            image: "https://http.cat/411.jpg",
-                            isCorrectAnswer: true
-                        },
-                        {
-                            text: "Texto da resposta 2",
-                            image: "https://http.cat/412.jpg",
-                            isCorrectAnswer: false
-                        }
-                    ]
-                }
-            ],
-            levels: [
-                {
-                    title: "Título do nível 1",
-                    image: "https://http.cat/411.jpg",
-                    text: "Descrição do nível 1",
-                    minValue: 0
-                },
-                {
-                    title: "Título do nível 2",
-                    image: "https://http.cat/412.jpg",
-                    text: "Descrição do nível 2",
-                    minValue: 50
-                }
-            ]
-        },
-        {
-            id: 2,
-            title: "Título do quizz2",
-            image: "https://http.cat/411.jpg",
-            questions: [
-                {
-                    title: "Título da pergunta 1",
-                    color: "#123456",
-                    answers: [
-                        {
-                            text: "Texto da resposta 1",
-                            image: "https://http.cat/411.jpg",
-                            isCorrectAnswer: true
-                        },
-                        {
-                            text: "Texto da resposta 2",
-                            image: "https://http.cat/412.jpg",
-                            isCorrectAnswer: false
-                        }
-                    ]
-                },
-                {
-                    title: "Título da pergunta 2",
-                    color: "#123456",
-                    answers: [
-                        {
-                            text: "Texto da resposta 1",
-                            image: "https://http.cat/411.jpg",
-                            isCorrectAnswer: true
-                        },
-                        {
-                            text: "Texto da resposta 2",
-                            image: "https://http.cat/412.jpg",
-                            isCorrectAnswer: false
-                        }
-                    ]
-                },
-                {
-                    title: "Título da pergunta 3",
-                    color: "#123456",
-                    answers: [
-                        {
-                            text: "Texto da resposta 1",
-                            image: "https://http.cat/411.jpg",
-                            isCorrectAnswer: true
-                        },
-                        {
-                            text: "Texto da resposta 2",
-                            image: "https://http.cat/412.jpg",
-                            isCorrectAnswer: false
-                        }
-                    ]
-                }
-            ],
-            levels: [
-                {
-                    title: "Título do nível 1",
-                    image: "https://http.cat/411.jpg",
-                    text: "Descrição do nível 1",
-                    minValue: 0
-                },
-                {
-                    title: "Título do nível 2",
-                    image: "https://http.cat/412.jpg",
-                    text: "Descrição do nível 2",
-                    minValue: 50
-                }
-            ]
-        }
-    ]
-    counter=0;
-    counterRespostas=0;
-    let apiSelector=objeto[0].questions;
-    let containerQuiz=document.querySelector(".page2 .quizz");
-    let pergunta;
-    while (counter < apiSelector.length){
-        // esse 0 vai ser o id
-        counterRespostas=0
-        containerQuiz.innerHTML+=`
-        <li class="quizQuestion pergunta${counter}">
-        <h3>${apiSelector[counter].title}</h3>
-        </li>
-        `
-        while(counterRespostas < apiSelector[counter].answers.length){
-            pergunta=document.querySelector(`.pergunta${counter}`);
-            pergunta.innerHTML +=`
-                <div>
-                    <img src="${apiSelector[counter].answers[counterRespostas].image}" alt="opção${counterRespostas}">
-                    <h4>${apiSelector[counter].answers[counterRespostas].text}</h4>
-                </div>
+    const promise=axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes')
+    promise.then(function(response){
+        counter=0;
+        counterRespostas=0;
+        let posicaoID=0
+        let questionSelector=response.data[posicaoID].questions;
+        let containerQuiz=document.querySelector(".page2 .quizzContainer");
+        let respostaSelector=questionSelector.answers;
+        respostaSelector=respostaSelector.sort()
+        while (counter < questionSelector.length){
+            counterRespostas=0
+            containerQuiz.innerHTML+=`
+            <li class="quizQuestion pergunta${counter}">
+                <h3>${questionSelector[counter].title}</h3>
+            </li>
             `
-            counterRespostas++
+            while(counterRespostas < questionSelector[counter].answers.length){
+                pergunta=document.querySelector(`.pergunta${counter}`);
+                pergunta.innerHTML +=`
+                <div>
+                    <img src="${questionSelector[counter].answers[counterRespostas].image}" alt="opção${counterRespostas}">
+                    <h4>${questionSelector[counter].answers[counterRespostas].text}</h4>
+                </div>
+                `
+                counterRespostas++
+            }
+            counter++
         }
-        counter++
-    }
-    counter=0
-    
-
+        counter=0
+    })
 }
