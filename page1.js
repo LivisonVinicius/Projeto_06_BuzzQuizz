11384e65953c7a03ce91a5279ee220c737e877ed
Mary Karakida <makarakida@gmail.com>
2022-04-20 00:08:40 -0300

    botoes de navegacao entre telas

--- page1.js
+++ page1.js
@@ -30,3 +30,29 @@ function renderizarQuizzesTodos(quizzesServidor) {
         lista.innerHTML += `<li class="quizz"><img src=${image} alt=""><div class="gradiente"></div><h3>${title}</h3></li>`
     }
 }
+
+function trocarTela1Tela2() {
+    document.querySelector(".page1").classList.toggle("escondido");
+    document.querySelector(".page2").classList.toggle("escondido");
+}
+function trocarTela1Tela3() {
+    document.querySelector(".page1").classList.toggle("escondido");
+    document.querySelector(".page3").classList.toggle("escondido");
+}
+function trocarTela2Tela3() {
+    document.querySelector(".page2").classList.toggle("escondido");
+    document.querySelector(".page3").classList.toggle("escondido");
+}
+
+function trocarEtapaI() {
+    document.querySelector(".etapa--inicial").classList.toggle("escondido");
+    document.querySelector(".etapa--perguntas").classList.toggle("escondido");
+}
+function trocarEtapaII() {
+    document.querySelector(".etapa--perguntas").classList.toggle("escondido");
+    document.querySelector(".etapa--niveis").classList.toggle("escondido");
+}
+function trocarEtapaIII() {
+    document.querySelector(".etapa--niveis").classList.toggle("escondido");
+    document.querySelector(".etapa--final").classList.toggle("escondido");
+}
