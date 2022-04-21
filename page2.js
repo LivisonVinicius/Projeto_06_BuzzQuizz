6fa82f9048a2783da2c331328e4894c59ba1d544
LivisonVinicius <livisongc@gmail.com>
2022-04-20 23:13:39 -0300

    lógica de finalizar o jogo falha

--- page2.js
+++ page2.js
@@ -5,6 +5,8 @@ let respostaSelector;
 let promise;
 let questionSelector;
 let pontuacao=0;
+let finalSelector;
+let mainPage2;
 function geraQuiz(){
     promise=axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes')
     promise.then(function(response){
@@ -12,9 +14,11 @@ function geraQuiz(){
         <header><h1>BuzzQuizz</h1></header>
         <main class="page2"></main>
         `
+        mainPage2=document.querySelector("main");
         counter=0;
         counterRespostas=0;
         let posicaoID=0
+        finalSelector=response.data[posicaoID].levels
         questionSelector=response.data[posicaoID].questions;
         document.querySelector(".page2").innerHTML+=`
         <li class="quizzTitle">
@@ -60,7 +64,9 @@ function selecionaResposta(selecionada,localPergunta){
         switch(questionSelector[posicaoPergunta].answers[contadorLoop].isCorrectAnswer){
             case true:
                 document.querySelector(`.${localPergunta} .resposta${contadorLoop}`).classList.add("truthy")
-                pontuacao++
+                if(selecionada.classList.contains("truthy")){
+                    pontuacao++
+                }
                 break
             case false:
                 document.querySelector(`.${localPergunta} .resposta${contadorLoop}`).classList.add("falsy")
@@ -68,7 +74,6 @@ function selecionaResposta(selecionada,localPergunta){
         contadorLoop++
     }
     selecionada.classList.remove("esbraquicado")
-
     setTimeout(scrollNext,2000,document.querySelector(`.pergunta${posicaoPergunta+1}`))
 }
 function parametro(){
@@ -78,10 +83,32 @@ function scrollNext(local){
     if (local!== null){
         local.scrollIntoView({behavior : "smooth", block : "end"})
     }else{
+        let arr = []
+        let posicaoLvl=0
+        let arrPosicao=[]
+        pontuacao=Math.floor((pontuacao/document.querySelectorAll(".quizQuestion").length)*100)
+        console.log(pontuacao)
+        for(let i=0 ; i<finalSelector.length;i++){
+            if(pontuacao>=finalSelector[i].minValue){
+                arr.push(finalSelector[i].minValue)
+                arrPosicao.push(finalSelector[i])
+            } 
+        }
+        novoArr=arr.sort(function(a, b){return a-b});
+        console.log(novoArr)
+        for(let i=0 ; i < arr.length ; i++){
+            if(novoArr[-1]==arr[i]){
+                arrPosicao=arrPosicao[i]
+            }
+        }
+        console.log(posicaoLvl)
         document.querySelector(".page2").innerHTML+=`
-        <div>
+        <div class="finalDoJogo">
+            <h3>${arrPosicao.title}</h3>
+            <img src="${arrPosicao.image}" alt="Imagem Final">
+            <h4>${arrPosicao.text}</h4>
+        </div>
         `
+        document.querySelector("finalDoJogo").scrollIntoView({behavior : "smooth", block : "end"})
     }
-
-
 }
